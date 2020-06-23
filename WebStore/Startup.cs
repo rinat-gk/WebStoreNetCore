95637da505277cf2110f805b5cb7cf40c3fa8e3a
rinat-gk <rinatgk01@gmail.com>
2020-06-24 02:06:50 +0400

    Modify Hello

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -32,7 +32,7 @@ namespace WebStore
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello beauty World!");
                 });
             });
         }

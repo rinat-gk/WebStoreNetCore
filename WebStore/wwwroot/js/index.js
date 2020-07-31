2c11d5283e23687b82749b4973efa2b3c0f9a867
rinat-gk <rinatgk01@gmail.com>
2020-08-01 01:50:07 +0400

    Add JS Event

--- WebStore/wwwroot/js/index.js
+++ WebStore/wwwroot/js/index.js
@@ -5,3 +5,12 @@ console.log("Hello My E-Store");
 
 var theForm = document.getElementById("TheForm");
 theForm.hidden = true;
+
+
+var button = document.getElementById("BuyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var ProductInfo = document.getElementsByClassName("product-props");
+var listItems = ProductInfo.item[0].children;

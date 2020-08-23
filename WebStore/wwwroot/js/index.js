d06bd7030cbd461d7b6a7e2320f2bb2f684ca0bc
rinat-gk <rinatgk01@gmail.com>
2020-08-24 01:14:43 +0400

    Do manipulations with Jquery, add configs in css

--- WebStore/wwwroot/js/index.js
+++ WebStore/wwwroot/js/index.js
@@ -1,20 +1,33 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
 
-console.log("Hello My E-Store");
-
-
-
-var theForm = $("#TheForm");
-theForm.hidden = true;
+    console.log("Hello My E-Store");
 
 
-var button = $("#BuyButton");
-button.on("click", function () {
-    console.log("Buying Item");
+
+    var theForm = $("#TheForm");
+    theForm.hidden = true;
+
+
+    var button = $("#BuyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var ProductInfo = $(".product-props li");
+    ProductInfo.on("click", function () {
+        console.log("You clicked on" + $(this).text());
+    });
+
+    var $loginToogle = $("#loginToogle");
+    var $popupForm = $(".popup-form");
+
+    $loginToogle.on("click", function () {
+        $popupForm.toggle(1000);
+    });
+
+
+
+
 });
-
-var ProductInfo = $(".product-props li");
-ProductInfo.on("click", function () {
-    console.log("You clicked on" + $(this).text());
-})
